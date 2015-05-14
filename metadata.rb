d2ca0a70be1571029b32f6bb013a2ad1abc6521c
Allen Sanabria <asanabria@linuxdynasty.org>
2015-05-13 23:52:22 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures rethinkdb-chef'
 long_description 'Installs/Configures rethinkdb-chef'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
